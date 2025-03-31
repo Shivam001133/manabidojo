59a74225239ef8d318f2837a94fca954e8f65d55
shivam <premashivam0011@gmail.com>
2025-03-31 17:28:18 +0530

    static config fix

--- config/settings/environment/production.py
+++ config/settings/environment/production.py
@@ -1,3 +1,4 @@
+import os
 from ..common import *
 from ..database import postgres
 from ..helpers.cloudflare.settings import CLOUDFLARE_R2_CONFIG_OPTIONS
@@ -9,7 +10,9 @@ db_postgres = postgres()
 DATABASES = {"default": db_postgres}
 
 # Static config
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = f"{CLOUDFLARE_R2_CONFIG_OPTIONS['endpoint_url']}/{CLOUDFLARE_R2_CONFIG_OPTIONS['bucket_name']}/static/"
+# media config
 MEDIA_URL = f"{CLOUDFLARE_R2_CONFIG_OPTIONS['endpoint_url']}/{CLOUDFLARE_R2_CONFIG_OPTIONS['bucket_name']}/media/"
 
 STORAGES = {
